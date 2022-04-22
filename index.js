097a165d4c303e484c0113f0bf501a667b70c069
yesim <sityesim@gmail.com>
2022-04-22 20:29:35 +0300

    completed assignmnent

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(driversList,string){
+
+    return driversList.filter((match) => match.toLowerCase() === string.toLowerCase());
+}
+
+
+function fuzzyMatch(driversList,string){
+    return driversList.filter ((match) =>
+    match.toLowerCase().indexOf(string.toLowerCase()) === 0
+);
+};
+
+
+
+function matchName(driversList, driverName ){
+    return driversList.filter((matched) => matched.name === driverName );
+}
